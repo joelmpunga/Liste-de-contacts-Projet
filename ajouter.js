edb90d240273c162351aada3d1f82865ded73a7a
Joel MPUNGA <joelmpunga79@gmail.com>
2023-11-27 22:14:03 +0200

    affichage d'un contact créé

--- ajouter.js
+++ ajouter.js
@@ -1,4 +1,14 @@
-let table = [];
+let table = [
+    {
+        prenom: "Joel",
+        nom: "MPUNGA",
+        telephone: "0842672114",
+        groupe:"L1",
+        email: "joel@gmail.com",
+        bio:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste aperiam repudiandae voluptatem officia voluptate excepturi eligendi mollitia et. Adipisci qui tenetur ipsam perferendis laborum, tempora assumenda totam quidem consectetur optio reiciendis sint expedita aspernatur, officia commodi enim veniam temporibus ab, fuga alias? Aspernatur eius nesciunt aliquid earum odio dolorem nobis. ",
+        image:"user-austin-ortiz-270x270.jpg",
+    },
+];
 let imageEnCours="";
 function addContact(table){
     const contact = {
@@ -30,12 +40,11 @@ inputImage.addEventListener('drop',function(e){
     e.stopPropagation();
     e.preventDefault();
     const fileList = e.dataTransfer.files;
-    let longeur = fileList.length-1;
-    let file = fileList[longeur];
+    let longeur = fileList.length;
+    let file = fileList[longeur-1];
     const name = file.name;
     const size = file.size;
     const ext = file.type;
-    console.log(fileList);
     divEnfant.style.display="block";
     let span = document.createElement('span');
     if(erreurImage){
@@ -53,10 +62,17 @@ inputImage.addEventListener('drop',function(e){
         container.appendChild(span);
     }
     else if(size>5000000){
-        console.log(size);
+        inputImage.setAttribute('style','border-color:red;');
+        //cette ligne ne doit s'executer si l'erreur est la
+        if(!erreurImage){
+            inputImage.lastChild.remove();
+        }
+        span.innerHTML= "La taille de l'image doit etre inferieur à 5MB";
+        span.style.color = "red";
+        erreurImage=true;
+        container.appendChild(span);
     }
     else{
-        console.log("autorisé");
         divEnfant.style.display="none";
         if(!erreurImage){
             container.lastChild.remove;
@@ -70,12 +86,86 @@ inputImage.addEventListener('drop',function(e){
 
 function afficherImage(div,name){
     let image = document.createElement('img');
-    let style = div.classList;
     image.src = name;
-    //image.classList=style;
     image.setAttribute('style','width:50%;height:100%;margin:auto;');
     image.alt="La photo deposée est ici";
     div.appendChild(image);
-    console.log("image affiché");
-    console.log(style);
 }
+
+
+function isEmailExits(email,contacts){
+    if(contacts.length==0){
+        return false;
+    }
+    else{
+        for(const contact of contacts){
+            if(contact.email==email){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+function viewsContact(table){
+    let longueur = table.length;
+    let dernierContact = table[longueur-1];
+    let container_oneContact = document.createElement('div');
+    container_oneContact.classList = "container-oneContact";
+    let container_image_contact = document.createElement('div');
+    container_image_contact.classList = "container-image-contact";
+    let container_detail_contact = document.createElement('div');
+    container_detail_contact.classList = "container-detail-contact";
+    let container_entete = document.createElement('div');
+    container_entete.classList = "container-entete";
+    let identite_oneContact = document.createElement('h4');
+    identite_oneContact.classList = "identite-oneContact";
+    identite_oneContact.innerHTML = dernierContact.prenom + " - " +dernierContact.nom+ " - " +dernierContact.groupe
+    let container_icones_oneContact = document.createElement('div');
+    container_icones_oneContact.classList="container-icones-oneContact"
+    let aModif = document.createElement('a');
+    aModif.href=""
+    aModif.addEventListener('click',function(){
+        //afficherAvantModif(longueur-1)
+    })
+
+    let faModif = document.createElement('i');
+    faModif.classList="fa fa-user"
+
+    let aSuppr = document.createElement('a');
+    aSuppr.href=""
+    aSuppr.addEventListener('click',function(){
+        //supprimer(longueur-1)
+    })
+
+    let faSuppr = document.createElement('i');
+    faSuppr.classList="fa fa-trash-o"
+
+    let numero_oneContact = document.createElement('div');
+    numero_oneContact.classList="numero-oneContact"
+
+    let bio_oneContact = document.createElement('div');
+    bio_oneContact.classList="bio-oneContact"
+
+    let contenaire_principale = document.querySelector('.container-cont');
+    contenaire_principale.appendChild(container_oneContact);
+    container_oneContact.appendChild(container_image_contact);
+    afficherImage(container_image_contact,dernierContact.image)
+    container_oneContact.appendChild(container_detail_contact);
+    container_detail_contact.appendChild(container_entete);
+    container_entete.appendChild(identite_oneContact);
+    container_entete.appendChild(container_icones_oneContact);
+    container_icones_oneContact.appendChild(aModif)
+    aModif.appendChild(faModif)
+    container_icones_oneContact.appendChild(aSuppr)
+    aSuppr.appendChild(faSuppr)
+    container_detail_contact.appendChild(numero_oneContact);
+    numero_oneContact.innerHTML=dernierContact.telephone;
+    container_detail_contact.appendChild(bio_oneContact);
+    bio_oneContact.innerHTML=dernierContact.bio;
+}
+
+// let rep=isEmailExits("joel@gmail.com",table);
+// alert(rep);
+
+viewsContact(table)
